df8c31d0ab8a4b983d0c1d9057f493bc52e8f35c
Galenso Serrano <galensoserrano1.com>
2024-12-05 17:09:37 -0300

    proceso de migracion de alojamiento hacia Vercel:deploy-22-14

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const express = require('express');
-const app = require('../index'); // Importa tu app de Express desde el archivo principal
+// const express = require('express');
+const app = require('../index.js'); // Importa tu app de Express desde el archivo principal
 
 module.exports = app; // Exporta la app completa
