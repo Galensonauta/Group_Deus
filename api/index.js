918ce0bc49ef79b999288482a8119617b8aa137d
Galenso Serrano <galensoserrano1.com>
2024-12-05 21:35:45 -0300

    proceso de migracion de alojamiento hacia Vercel:deploy-22-15(+ pruebas en vercel.json)

--- api/index.js
+++ api/index.js
@@ -1,13 +1,13 @@
-const express = require('express');
+// const express = require('express');
 const app = require("../index");
 
-// Middleware básico
-app.use(express.json());
+// // Middleware básico
+// app.use(express.json());
 
-// Endpoint de prueba
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'API funcionando correctamente en Vercel' });
-});
+// // Endpoint de prueba
+// app.get('/', (req, res) => {
+//   res.status(200).json({ message: 'API funcionando correctamente en Vercel' });
+// });
 
 // Exporta la app
 module.exports = app;
