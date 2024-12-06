5eb2afc490023ac133a507ce9fe47155004a675f
Galenso Serrano <galensoserrano1.com>
2024-12-06 19:06:57 -0300

    proceso de migracion de alojamiento hacia Vercel:deploy-22-22(copy index a index in api)

--- api/index.js
+++ api/index.js
@@ -1,13 +1,61 @@
-const express = require('express');
-const app = require("../index");
 
-// Middleware básico
-app.use(express.json());
+const express = require('express'); // Requiere librería express
+const { json } = express; // Extrae 'json' del módulo 'express'
+const cors = require('cors');
+const routerApi = require('../routes/index.js');
+require('dotenv').config();
+const { checkApiKey } = require('../middlewares/authHandler.js');
 
-// Endpoint de prueba
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'API funcionando correctamente en Vercel' });
-});
 
-// Exporta la app
-module.exports = app;
+const cookieParser = require('cookie-parser');
+const path = require('path');
+
+
+// Acceder a la arquitectura
+// Requiere los middlewares desde el archivo
+const { logErrors, 
+  errorHandler, 
+  boomErrorHandler, 
+  ormErrorHandler } = require('../middlewares/errorHandler.js');
+
+const app = express(); // Crear instancia de aplicacion de express
+
+
+app.use(cookieParser());
+
+app.use(json())
+const whiteList=["http://localhost:8080","https://group-deus.vercel.app"]
+const options={
+  origin: (origin, callback)=>{
+    if(whiteList.includes(origin)||!origin){
+      callback(null,true)
+    }else{
+      callback(new Error("No permitido"))
+    }
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PATCH', 'DELETE']
+}
+app.use(cors(options))
+
+
+app.get('/nueva-ruta', checkApiKey,(req, res) => {
+  // Respuesta al cliente
+     res.send('Hola , soy una nueva ruta');
+   });
+  
+   // Ruta para cualquier solicitud no manejada
+   app.get('*', (req, res) => {
+     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+   });
+   
+// const passport=
+require("./utils/auth")
+// app.use(passport.initialize());
+routerApi(app);
+app.use(logErrors)
+app.use(ormErrorHandler)
+app.use(boomErrorHandler)
+app.use(errorHandler)
+// Middleware para servir archivos estáticos del frontend
+app.use(express.static(path.join(__dirname, 'dist')));
