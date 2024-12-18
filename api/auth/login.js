2f30a788d80d9fd518453abfefc291a512f9b2fb
Galenso Serrano <galensoserrano1.com>
2024-12-17 21:12:19 -0300

    Ruta new para user + login funcionando correctamente. Las estrategias de jwt se fusionaron en index.para gestionar el token en las cookies se cambio el uso de express por la biblioteca express(para serverless).para usar passport (aplica a todos los middlewares) se los envolvio en otra funcion: runMiddlewares.para validar esquemas en la ruta new se reemplazo el uso del middleware validatorHandler por el uso del esquema de forma explicita : createUserScehma.valite

--- api/auth/login.js
+++ api/auth/login.js
@@ -1,40 +1,63 @@
-// require("../../utils/auth")
-// const util = require('util');
+// const authenticate = util.promisify(passport.authenticate('local', { session: false }));
+const cookie = require('cookie');
 const jwt = require("jsonwebtoken")
 const {config}=require("../../config/config")
 const passport=require("passport")
-// const authenticate = util.promisify(passport.authenticate('local', { session: false }));
-module.exports = (req, res) => {
-  passport.authenticate('local', { session: false }, (err, user, info) => {
-    console.log('Resultado de Passport:', { err, user, info });
+require("../../utils/auth")
 
-    if (err) {
-      console.error('Error en Passport:', err);
-      return res.status(500).json({ message: 'Error interno del servidor' });
-    }
-    if (!user) {
-      console.log('Información de Passport:', info);
-      return res.status(401).json({ message: 'Credenciales incorrectas' });
-    }
+// Utilidad para envolver middlewares
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
+module.exports = async (req, res, next) => {  
     try {
+      await runMiddleware(req, res, passport.authenticate('local', { session: false }));
+      const user = req.user
+      if (!user) {
+        return res.status(401).json({ message: 'Credenciales incorrectas' });
+      }
+      console.log('Usuario autenticado:', user);
       // Crear el payload del token
       const payload = {
         sub: user.id,
         role: user.role
       };
-
       // Generar el token JWT
       const token = jwt.sign(payload, config.jwtSecret, { expiresIn: '1h' });
+      //Configuración de la cookie con la biblioteca cookie: adaptación para entorno serveless
+     // Serializar la cookie con la biblioteca cookie
+     const serializedCookie = cookie.serialize('token', token, {
+      httpOnly: true,
+      secure: true, // Cambiar a true si usas HTTPS
+      sameSite: 'lax',
+      maxAge: 60 * 60, // 1 hora
+      path: '/', // Cookie disponible en toda la aplicación
+    });
 
-      // Configurar la cookie con el token
-      res.cookie('token', token, {
-        httpOnly: true, // Protege la cookie de ser accesible por JavaScript
-        secure: true, // Cambiar a `true` en producción con HTTPS
-        sameSite: 'Lax', // Protección CSRF
-        maxAge: 60 * 60 * 1000 // 1 hora
-      });
+    res.setHeader('Set-Cookie', serializedCookie);
+    console.log("el token es:", serializedCookie)
+      // Configurar la cookie con el token con res.cookies de express
+      // res.cookie('token', token, {
+      //   httpOnly: true, // Protege la cookie de ser accesible por JavaScript
+      //   secure: true, // Cambiar a `true` en producción con HTTPS
+      //   sameSite: 'Lax', // Protección CSRF
+      //   maxAge: 60 * 60 * 1000 // 1 hora
+      // });
+      // console.log('Token generado:', token);
 
-      console.log('Token generado:', token);
+        // Llamar a `next` si es necesario
+    if (next) {
+      return next();
+    }
+
       return res.json({
         message: 'Inicio de sesión exitoso',
         user: {
@@ -46,9 +69,11 @@ module.exports = (req, res) => {
       });
     } catch (err) {
       console.error('Error al generar el token:', err);
+      if (next) {
+        return next(err); // Pasar el error al middleware de manejo de errores
+      }
       return res.status(500).json({ message: 'Error interno del servidor' });
-    }
-  })(req, res); // Llamar al middleware de Passport
+    }  
 };
 // module.exports= async(req,res)=>{
 //           try{
