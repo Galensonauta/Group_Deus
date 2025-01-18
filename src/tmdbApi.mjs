09284d83b4b722e439a62e16fd1f6fe1006b6e46
Galenso Serrano <galensoserrano1.com>
2025-01-18 18:56:07 -0300

    prueba para llamar api key desde el backend-3

--- src/tmdbApi.mjs
+++ src/tmdbApi.mjs
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://group-deus-backend-express.onrender.com/api',  // Opcionalmente, establece una URL base para tus solicitudes
+  baseURL: 'https://group-deus-backend-express.onrender.com/',  // Opcionalmente, establece una URL base para tus solicitudes
   withCredentials: true,
 });
 
